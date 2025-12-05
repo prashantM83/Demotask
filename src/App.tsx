8b7f4c84660b4d29d7e69eba77e40fcf8432492f
Prashant Brijesh Mishra <prashantm9181@gmail.com>
2025-12-05 13:14:18 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,26 +4,7 @@ export default function DemoApp() {
   const [num1, setNum1] = useState(0);
   const [num2, setNum2] = useState(0);
   const [result, setResult] = useState<number | string>(0);
-  //const [PLUS, setPLUS] = useState(0);
 
-  // useEffect(() => {
-  //   console.log("Incrementing And Decrementing");
-  //   function operation() {
-  //     for (let i = 1; i <=10; i++) {
-  //       if(i<=10){
-  //         setPLUS(PLUS + i);
-  //         console.log(PLUS);
-  //       }
-  //       for (let i = 10; i <=1; i--) {
-  //         if(i<=10){
-  //           setPLUS(PLUS - i);
-  //           console.log(PLUS);
-  //         }
-  //       }  
-  //     }
-  //   }
-  //   operation();    
-  // },[]) 
   function add() {
     setResult(num1 + num2);
   }
@@ -61,13 +42,11 @@ export default function DemoApp() {
       <button onClick={mul}>Multiply</button>
       <button onClick={div}>Divide</button>
       <h2>Result: {result}</h2>
-      {/* <h2>PLUS Value: {PLUS}</h2> */}
-      <div>
+      
+    
       <h2>My React App</h2>
-
-      {/* Use your Products component */}
       <Products />
-    </div>
+      
 
     </div>
   );
