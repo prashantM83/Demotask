3ae06de2ba33b40bf3b7e2f4effc350752dcad83
Prashant Brijesh Mishra <prashantm9181@gmail.com>
2025-12-08 16:12:37 +0530

    added css

--- src/screens/product.tsx
+++ src/screens/product.tsx
@@ -1,37 +1,68 @@
 import { useState } from "react";
 
+const styles = {
+  container: {
+    padding: 20,
+  },
+  hello: {
+    backgroundColor: "blue",
+    color: "white",
+    padding: "10px",
+    borderRadius: "5px",
+  },
+};
 type Product = {
   id: number;
   title: string;
   price: number;
   description: string;
+  images: string;
 };
 
 export default function Products() {
   const [products, setProducts] = useState<Product[]>([]);
+  const [selectedProduct, setSelectedProduct] = useState<Product | null>(null);
 
   const fetchProducts = () => {
     fetch("https://dummyjson.com/products?limit=10")
       .then((res) => res.json())
       .then((data) => {
-        setProducts(data.products);   
+        setProducts(data.products);
       });
   };
 
+  if (selectedProduct) {
+    return (
+      <div style={styles.container}>
+        <button onClick={() => setSelectedProduct(null)}>Back</button>
+
+        <h2>{selectedProduct.title}</h2>
+        <img src={selectedProduct.images[0]} height={150} alt="" />
+        <p>Price: {selectedProduct.price}</p>
+        <p>{selectedProduct.description}</p>
+      </div>
+    );
+  }
+
   return (
-    <div>
-      <button onClick={fetchProducts}>Get Product Details</button>
+    <div style={styles.container}>
+      <button style={styles.hello} onClick={fetchProducts}>
+        Get Products Details
+      </button>
 
       <ul>
         {products.map((p) => (
-          <li>
+          <li key={p.id}>
             <h3>{p.title}</h3>
-            <p>Price: {p.price}</p>
-            <p>{p.description}</p>
+
+            <img
+              src={p.images}
+              height={100}
+              onClick={() => setSelectedProduct(p)}
+            />
           </li>
         ))}
       </ul>
     </div>
   );
 }
-
